fdf7965a66417039f25114d7638f5ec4d30023df
Brayhan E. Contreras <bcontreras@MAGNA_DOMAIN.COM>
2019-05-17 14:20:38 -0400

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://127.0.0.1/local_library';
+var dev_db_url = 'mongodb://127.0.0.1/local_library';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
